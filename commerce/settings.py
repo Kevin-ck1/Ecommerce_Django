5c80288668d7d19c80f1506265bddff9bb379a03
Kevin Mutinda <kevin.mutinda.ck@gmail.com>
2023-03-06 12:23:12 +0300

    Database Reverted

--- commerce/settings.py
+++ commerce/settings.py
@@ -80,21 +80,21 @@ WSGI_APPLICATION = 'commerce.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 #We are to copy out the sql database
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
 
+'''
 #Render PostgreSql database(live)
 import dj_database_url
 DATABASES = {
     'default': dj_database_url.parse(env('DATABASE_URL'))
 }
-
+'''
 AUTH_USER_MODEL = 'auctions.User'
 
 # Password validation
